0f3d3be0c48df5d478cea461b958729613f0c66b
xuzepei <xuzepei@gmail.com>
2014-01-21 17:58:18 +0800

    fixed register form

--- index_1.php
+++ index_1.php
@@ -7,7 +7,7 @@
         <meta name="author" content="">
         <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
 
-        <title>用户登录</title>
+        <title>设计师列表</title>
 
         <!-- Bootstrap core CSS -->
         <link href="./bootstrap/v3/css/bootstrap.css" rel="stylesheet">
@@ -16,7 +16,7 @@
         <link href="./css/designer_list.css" rel="stylesheet">
         
         <!-- My JS -->
-        <script src="./js/login_validate.js"></script>
+        <script src="./js/register_validate.js"></script>
 
         <!-- Just for debugging purposes. Don't actually copy this line! -->
         <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
@@ -43,10 +43,17 @@
                 </div>
                 <div class="navbar-collapse collapse">
                 <p class="navbar-text pull-right">
-              <?php
+                    
+                                        <?php
+
 require_once './config.php';
-echo '<a href='.'"'.$base_url.'/register.php'.'"'.'class="navbar-link">注册</a>';
+require_once BASE_PATH.'/error_log_setting.php';
+echo '<a href='.'"'.$base_url.'/login.php'.'"'.'class="navbar-link">登录</a>   |   '.'<a href='.'"'.$base_url.'/register.php'.'"'.'class="navbar-link">注册</a>';
+
                     ?>
+                    
+              <!--<a href="#" class="navbar-link">登录</a> | 
+              <a href="#" class="navbar-link">注册</a>-->
             </p>
                 </div><!--/.nav-collapse -->
             </div>
@@ -58,67 +65,20 @@ echo '<a href='.'"'.$base_url.'/register.php'.'"'.'class="navbar-link">注册</a
             <!-- Begin page content -->
             <div class="container">
                 <div class="page-header">
-                    <h1>用户登录</h1>
+                    <h1>欢迎</h1>
                 </div>
                 <p>
 
                     <!-- list -->
                     <?php
+//                    include_once("mvc/controller/DesignerController.php");
+//                    $controller = new DesignerController();
+//                    $controller->invoke();
+
 require_once './config.php';
 require_once BASE_PATH.'/error_log_setting.php';
-require_once BASE_PATH.'/db/medoo.php';
-$database = new medoo('gongzhang_db');
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-$user_tb = "user_tb";
 
 
-$current_date = date('Y-m-d H:i:s',time());
-
-if(!empty($username))
-{ 
-    $sql = 'select * from '.$user_tb.' where username = "'.$username.'" && '.'password = "'.$password.'"';
-    $result = $database->select_by_sql($sql);
-    
-    //var_dump($result);
-    //var_dump($database->error());
-    
-    $msg = '';
-    $logined = false;
-    $by_type = 'username';
-    if($result != false && count($result,0))
-    {
-        $msg = 'Can login by username!';
-        $logined = true;
-    }
-    else
-    {
-        $by_type = 'email';
-        $sql = 'select * from '.$user_tb.' where email = "'.$username.'" && '.'password = "'.$password.'"';
-        $result = $database->select_by_sql($sql);
-        
-        if($result != false && count($result,0))
-        {
-            $msg = 'Can login by email!';
-            $logined = true;
-        }
-    }
-    
-    $msg = 'Can not login!';
-    
-    if($logined)
-    {
-        // 更新用户登录信息
-        $ip = $_SERVER['REMOTE_ADDR']; // 获取客户端的IP
-        $sql = 'update '.$user_tb.' set login_times = login_times + 1, last_login_time = "'.$current_date.'", login_ip = "'.$ip.'" where '.$by_type.' = "'.$username.'"';
-        //echo $sql;
-        $result = $database->exec($sql);
-        //echo 'result:'.$result;
-        //var_dump($database->error());
-    }
-    
-}
                     ?>
                     
                     
@@ -126,24 +86,6 @@ if(!empty($username))
                   <input type="submit" name="add_designer_button" id="add_designer_button" value="添加">
                 </form>-->
 
-<form name="login_form" method="post" action="login.php" onsubmit="return validate();">
-<table width="330" border="0" align="center" cellpadding="5" bgcolor= "#eeeeee">
-<tr>
-    <td>用户名/邮箱：</td>
-    <td><input name="username" type="text" id="username"> </td>
-</tr>
-<tr>
-    <td>密码：</td>
-    <td><input name="password" type="password" id="password"></td>
-</tr>
-<tr>
-    <td colspan="2" align="center">
-      <input type="submit" name="submit" value="提交">
-</tr>
-</table>
-</form>
-
-                
                     <!-- footer -->
                 <hr>
                 <footer>
@@ -164,5 +106,4 @@ if(!empty($username))
 
 
 </body></html>
-
 
