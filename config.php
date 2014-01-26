e83e6d99dac5e42aa99284d9dad06203e42b11cc
xuzepei <xuzepei@gmail.com>
2014-01-26 18:05:37 +0800

    添加列表分页功能

--- config.php
+++ config.php
@@ -17,5 +17,6 @@ define('DB_HOST','127.0.0.1');
 define('DB_PORT',3306);
 define('DB_NAME','gongzhang_db');
 
-
+//默认查询单页数量
+define('PAGE_SIZE',20);
 ?>
--- mvc/controller/DesignerController.php
+++ mvc/controller/DesignerController.php
@@ -4,15 +4,16 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-include_once BASE_PATH.'/mvc/model/Designer.php';
+include_once BASE_PATH . '/mvc/model/Designer.php';
 
 class DesignerController {
-    
+
     public $db_name = DB_NAME;
     public $tb_name = 'designer_tb';
-        
+
     public function __construct()
     {
+        
     }
 
     public function invoke()
@@ -21,29 +22,38 @@ class DesignerController {
         $head1 = '设计师';
         $css_file_array = array('/bootstrap/v3/css/bootstrap.css');
 
-        include BASE_PATH.'/mvc/view/header.php';
-        
+        include BASE_PATH . '/mvc/view/header.php';
+
+        $page = $_GET['page'];
+        $count = $_GET['count'];
+        if (!isset($page) || $page <= 0)
+            $page = 1;
+        if (!isset($count) || $count < 0)
+            $count = PAGE_SIZE;
+
         $action = $_GET['action'];
         $id = $_GET['id'];
-        if(isset($id) && isset($action) && 0 == strcmp($action,"delete"))
+        if (isset($id) && isset($action) && 0 == strcmp($action, "delete"))
         {
             $this->removeById($id);
         }
 
-        $designers = $this->getItemList();
-        include BASE_PATH.'/mvc/view/designer_list.php';
-        include BASE_PATH.'/mvc/view/footer.php';
+        $designers = $this->getItems($page, $count);
+        include BASE_PATH . '/mvc/view/designer_list.php';
+        $this->page_controller($page, $count);
+        include BASE_PATH . '/mvc/view/footer.php';
     }
 
-    public function getItemList()
+    public function getItems($page, $count)
     {
-        require_once BASE_PATH.'/db/medoo.php';
+        require_once BASE_PATH . '/db/medoo.php';
         $database = new medoo($this->db_name);
-        $result = $database->select($this->tb_name, "*");
-        
+        $from_index = ($page - 1) * $count;
+        $result = $database->select_by_sql('select * from ' . $this->tb_name . ' limit ' . $from_index . ',' . $count);
+
         $designer_array;
         $i = 0;
-        foreach($result as $designer_info)
+        foreach ($result as $designer_info)
         {
             $designer_array[$i] = new Designer($designer_info);
             $i++;
@@ -51,24 +61,59 @@ class DesignerController {
         return $designer_array;
     }
 
+    public function getAllItems()
+    {
+        require_once BASE_PATH . '/db/medoo.php';
+        $database = new medoo($this->db_name);
+        $result = $database->select($this->tb_name, "*");
+
+        $designer_array;
+        $i = 0;
+        foreach ($result as $designer_info)
+        {
+            $designer_array[$i] = new Designer($designer_info);
+            $i++;
+        }
+        return $designer_array;
+    }
+
+    public function getItemsCount()
+    {
+        require_once BASE_PATH . '/db/medoo.php';
+        $database = new medoo($this->db_name);
+        $result = $database->count('select count(*) from ' . $this->tb_name);
+
+        return $result;
+    }
+
     public function getItem($id)
     {
-        $array = $this->getItemList();
-        return $array[$id];
+        require_once BASE_PATH . '/db/medoo.php';
+        $database = new medoo($this->db_name);
+        $result = $database->select_by_sql('select * from ' . $this->tb_name . ' where id=' . $id . ' limit 1');
+
+        $designer_array;
+        $i = 0;
+        foreach ($result as $designer_info)
+        {
+            $designer_array[$i] = new Designer($designer_info);
+            $i++;
+        }
+        return $designer_array;
     }
-    
+
     public function removeById($id)
     {
         //require_once 'error_log_setting.php';
-        require_once BASE_PATH.'/db/medoo.php';
+        require_once BASE_PATH . '/db/medoo.php';
         $database = new medoo($this->db_name);
-        $database->delete($this->tb_name, ["id"=>$id]);
+        $database->delete($this->tb_name, ["id" => $id]);
     }
 
     public function add($title, $header, $css_file_array, $js_file_array)
     {
-        include BASE_PATH.'/mvc/view/header.php';
-        
+        include BASE_PATH . '/mvc/view/header.php';
+
         $uploadImageFailed = false;
         $save_path;
         $head_image_url;
@@ -114,8 +159,8 @@ class DesignerController {
         if (isset($action) && !strcmp($action, "submit"))
         {
             $error_text = '';
-            
-            if(false == $uploadImageFailed && isset($save_path))
+
+            if (false == $uploadImageFailed && isset($save_path))
             {
                 $name = $_POST["name"];
                 if (isset($name) && strlen($name))
@@ -146,7 +191,7 @@ class DesignerController {
                         $head_image_url = '';
 
                     //上传成功，写入数据库
-                    require_once BASE_PATH.'db/medoo.php';
+                    require_once BASE_PATH . 'db/medoo.php';
                     $database = new medoo($this->db_name);
 
                     $result = $database->insert("designer_tb", [
@@ -157,14 +202,14 @@ class DesignerController {
                         "description" => $desc,
                         "head_image_url" => $head_image_url,
                         "type" => $type,
-                            ]);
+                    ]);
 
                     if ($result)
                     {
-                        echo '<div class="alert alert-success">'."已成功添加设计师！".'</div>';
-                        
+                        echo '<div class="alert alert-success">' . "已成功添加设计师！" . '</div>';
+
                         echo '<script type="text/javascript" >
-                            window.location.href="'."/index.php".'"; 
+                            window.location.href="' . "/index.php" . '"; 
                             </script>';
                     }
                 }
@@ -176,27 +221,86 @@ class DesignerController {
                 //echo '<script type="text/javascript" >alert("上传头像失败！");</script>';
                 $error_text = "添加设计师失败。提示：添加头像失败！";
             }
-            
-            if(strlen($error_text))
+
+            if (strlen($error_text))
             {
                 include 'mvc/view/add_designer_form.php';
-                
-                if(file_exists($save_path))
+
+                if (file_exists($save_path))
                 {
                     unlink($save_path);
                 }
-                echo '<div class="alert alert-danger">'.$error_text.'</div>';
+                echo '<div class="alert alert-danger">' . $error_text . '</div>';
             }
         }
         else
-            include BASE_PATH.'mvc/view/add_designer_form.php';
+            include BASE_PATH . 'mvc/view/add_designer_form.php';
 
-        include BASE_PATH.'mvc/view/footer.php';
+        include BASE_PATH . 'mvc/view/footer.php';
     }
-    
+
     public function test()
     {
-        echo 'test from '.__FILE__;
+        echo 'test from ' . __FILE__;
+    }
+
+    private function page_controller($page, $count)
+    {
+//            <ul class="pagination">
+//      <li><a href="#">&laquo;</a></li>
+//      <li><a href="#">1</a></li>
+//      <li><a href="#">2</a></li>
+//      <li><a href="#">3</a></li>
+//      <li><a href="#">4</a></li>
+//      <li><a href="#">5</a></li>
+//      <li><a href="#">&raquo;</a></li>
+//    </ul>
+
+        //页面数量
+        $sum = $this->getItemsCount();
+        $page_count = floor($sum / $count);
+        if ($sum % $count != 0)
+            $page_count += 1;
+        
+        if(1 == $page_count || $page > $page_count)
+            return;
+
+        $li_count = min($page_count, 5);//最多显示5个页面按钮
+
+        //前一页索引
+        $previous_page = max(0, $page - 1);
+        //后一页索引
+        $next_page = min($page_count, $page + 1);
+
+        $previous_li = '';
+        if ($page - 1 <= 0)
+            $previous_li = '<li class="disabled"><a href="' . BASE_URL . '?c=designer&page=' . $previous_page . '">&laquo;</a></li>';
+        else
+            $previous_li = '<li><a href="' . BASE_URL . '?c=designer&page=' . $previous_page . '">&laquo;</a></li>';
+
+        $base_index = floor(($page - 1)/ $li_count);
+        $lis = '';
+        for ($i = 1; $i < $li_count + 1; $i++)
+        {
+            $li = '';
+            $index = $base_index * $li_count + $i;
+            if($index > $page_count)
+                continue;
+            
+            if ($index == $page)
+                $li = '<li class="active"><a href="' . BASE_URL . '?c=designer&page=' . $index . '">' . $index . '</a></li>';
+            else
+                $li = '<li><a href="' . BASE_URL . '?c=designer&page=' . $index . '">' . $index . '</a></li>';
+            $lis .= $li;
+        }
+        
+        $next_li = '';
+        if ($page >= $page_count)
+            $next_li = '<li class="disabled"><a href="' . BASE_URL . '?c=designer&page=' . $next_page . '">&raquo;</a></li>';
+        else
+            $next_li = '<li><a href="' . BASE_URL . '?c=designer&page=' . $next_page . '">&raquo;</a></li>';
+
+        echo '<ul class="pagination">' . $previous_li . $lis .$next_li.'</ul>';
     }
 
 }
